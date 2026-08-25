f39e0c6c340515c773b1286cce9ff7059805b9e5
Kamil Schmidt <61392709+szmitek@users.noreply.github.com>
2026-08-25 21:23:46 +0200

    fix: use explicit animated layer positioning

--- src/ui/components/ExerciseGuide.tsx
+++ src/ui/components/ExerciseGuide.tsx
@@ -176,7 +176,11 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   poseLayer: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
     overflow: 'hidden',
   },
   poseSheet: {
